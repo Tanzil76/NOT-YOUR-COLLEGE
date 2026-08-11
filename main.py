fe7c155da57be69154e6f4e556c38313b686b9db
Tanzil <tanzilibrahim5@gmail.com>
2026-08-11 13:51:54 +0530

    update

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-#This is the first program
+
+#This is the line for printing
 print("hello NYC people")
+
+#Variables = #camelCase - myVariableName
+             #PascalCase - MyVariableName
+             #snake_case - my_variable_name
+
+NYC = 12
+print("NYC")
+
+
