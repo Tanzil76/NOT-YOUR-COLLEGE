187718ad18b9e9ebedef8453ae07b11c8ebc1fb8
Tanzil <tanzilibrahim5@gmail.com>
2026-08-11 14:09:09 +0530

    update

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+#Numbers -:
+
+a = -23
+b = 45
+c = 56
+
+#int - 0 to infinity and -1 to negative infinity
+print(type(c))
+
+#float - decimal and fraction number
+d = 12.1
+print(type(d))
+
+#complex - imaginary number
+e = 12 + 3j
+print(type(e))
+
+#String -: anything inside quotes- "" or ''
+
+name = 'NYC'
+print(type(name))
+print(name)
+
+#Boolean -: 
+
+g = True
+h = False
+
+print(type(g))
+print(type(h))
+
