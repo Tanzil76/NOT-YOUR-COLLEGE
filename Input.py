a3c7973c47987aee0b1ae2002cc67d0019edf657
Tanzil <tanzilibrahim5@gmail.com>
2026-08-11 18:13:19 +0530

    update

---
+++ Input.py
@@ -0,0 +1,18 @@
+name = "Tanzil"
+age = 23
+
+#print string using variable in string - print(f"string {variable} string {variable}") - formatted string
+#{}- python expect to provide some value
+print(f"Hi my name is {name} and my age is {age}") # Using f-string
+
+#other way - Using multiple values - print("string",varibale,"string",variable)
+print("Hi my name is",name,"and my age is",age)
+
+#Input -: input() always returns a string. If you need a number, convert it manually with int() or float().
+
+age = input("What is your age -: ")
+print(f"Hello your age is {age}")
+
+# converting age to integer due to some specific reason
+age = int(input("What is your age -: "))
+print(f"Hello your age is {age}")
