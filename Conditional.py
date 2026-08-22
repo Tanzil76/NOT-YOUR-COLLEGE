b15b2d87ed29773b47e792f349054f7afc0ba630
Tanzil <tanzilibrahim5@gmail.com>
2026-08-22 20:31:33 +0530

    new

--- Conditional.py
+++ Conditional.py
@@ -13,9 +13,24 @@ else:
 
 rupees = int(input(" Give money - "))
 if rupees == 10:
-    print("I will have a chcocobar")
+    print("I will have a chocobar")
 elif rupees == 50:
     print("I will have manchurian")
 elif rupees == 100:
     print("Mcd")
-    print("5 Star dhaba")        
+elif rupees == 500:
+    print("5 Star dhaba")       
+else:
+    print("Bhuka rahu ga mai") 
+
+# Solve :
+
+# 1. Accept two numbers and print the greatest between them.
+
+num1 = int(input("Please give me me first number :-"))
+num2 = int(input("Please give me me second number :-"))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
