21c48aed51db89b58e5fbe59af486ba09e7d6bb5
Tanzil <tanzilibrahim5@gmail.com>
2026-08-22 16:44:38 +0530

    new

--- Conditional.py
+++ Conditional.py
@@ -1,2 +1,21 @@
+#If-else statements -:
+
 if True:
-    print("My name is Tanzil")
+    print("My name is Tanzil") # My name is Tanzil
+
+# You have to input of age and tell the person can vote or not
+
+age = int(input("Please tell your age :-"))
+if age >= 18:
+    print("Hello brother you can vote")
+else:
+    print("Hello you can not vote sorry")
+
+rupees = int(input(" Give money - "))
+if rupees == 10:
+    print("I will have a chcocobar")
+elif rupees == 50:
+    print("I will have manchurian")
+elif rupees == 100:
+    print("Mcd")
+    print("5 Star dhaba")        
