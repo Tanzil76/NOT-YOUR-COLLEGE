62cd7e06f6772b2e745d442d1f7b5946da7a2ab5
Tanzil <tanzilibrahim5@gmail.com>
2026-08-23 20:14:00 +0530

    new

---
+++ Loops.py
@@ -0,0 +1,23 @@
+# Loops -:
+
+# range() function -: 
+
+    # range() generates a sequence numbers. Think of its saying "count from here to there"
+
+    # range(stop) - 0 up to stop-1
+    # range(start, stop) - start up to stop-1
+    # range(start, stop, step) - start, jumping by step
+
+range(10, 101, 1)
+range(23, 57, 1)
+range(46)
+
+# For loop -:
+
+for i in range(46):
+    print(i)
+
+# Print table of 5
+
+for i in range(5,51,5):
+    print(i)
