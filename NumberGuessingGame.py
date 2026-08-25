8cb085ba2c2ab91c3bd1e15bf3863ed9bc1f79b0
Tanzil <tanzilibrahim5@gmail.com>
2026-08-25 20:18:20 +0530

    new

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -2,12 +2,14 @@
 
 import random
 comp = random.randint(1,100)
+tries = 0
 
 while True:
+    tries = tries + 1
     huma = int(input("Guess your number between 1 - 100 -: "))
 
     if huma == comp:
-        print("Congratulations you have won ! ")
+        print(f"Congratulations you have won in {tries} tries ! ")
         break
     elif huma > comp:
         print("Sorry wrong guess go lower ! ")
