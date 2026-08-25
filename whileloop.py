847b2527783e37bc0bbc07ce8a6d9d2c6d1c00ac
Tanzil <tanzilibrahim5@gmail.com>
2026-08-25 19:29:52 +0530

    new

--- whileloop.py
+++ whileloop.py
@@ -1,3 +1,5 @@
+# While Loop -: the while loop keeps running as long as a condition is True. You use it when you don't know how many times you'll need to repeat.
+
 a = 1
 while a != 20:
     print(a)
