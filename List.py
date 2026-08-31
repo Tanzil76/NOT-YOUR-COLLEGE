c042d9f036f1e5b09bfa87025d26febadc0f687d
Tanzil <tanzilibrahim5@gmail.com>
2026-08-31 20:11:13 +0530

    new

--- List.py
+++ List.py
@@ -92,3 +92,16 @@ for i in l:
     sum = sum + i
 
 print(f"Average is {sum/len(l)}")
+
+# 3. Find the greatest element and print its index.
+
+l = [4,8,2,9,1]
+largest = l[0]
+index = 0
+
+for i in range(len(l)):
+    if l[i] > largest:
+        largest = l[i]
+        index = i
+
+print(f"Largest value is {largest} at index {index}")
