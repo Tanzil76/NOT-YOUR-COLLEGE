b5bebc8fa8dda5f4ea849ca95f08c301b431486b
Tanzil <tanzilibrahim5@gmail.com>
2026-08-31 17:29:22 +0530

    new

--- List.py
+++ List.py
@@ -29,6 +29,7 @@ for i in range(0,len(a)):     # len(a) - means length of a list
 # append method -: list.append(value) - adding new element in list in the end.
 
 a = [10,20,30,40,50]
-a.append(60)
+a.append(60)   # adds a value to the last spot
 a.append("Hello")
+a.insert(2,25)   # add values in the middle
 print(a) 
