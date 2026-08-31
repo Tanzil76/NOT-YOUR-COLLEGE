0856f38aa33dcf21e163a6c1cff9ea73d5e09e78
Tanzil <tanzilibrahim5@gmail.com>
2026-08-31 19:57:15 +0530

    new

--- List.py
+++ List.py
@@ -82,3 +82,13 @@ for i in l:
 
 print(f"Positive Elements -: {pos}")
 print(f"Neagtive Elements -: {neg}")
+
+# 2. Find the mean (average) of all list elements.
+
+l = [10,20,30,40]
+sum = 0
+
+for i in l:
+    sum = sum + i
+
+print(f"Average is {sum/len(l)}")
