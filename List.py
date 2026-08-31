5c258499ff4c5f271e19b39ccc642448472622a9
Tanzil <tanzilibrahim5@gmail.com>
2026-08-31 17:34:45 +0530

    new

--- List.py
+++ List.py
@@ -31,11 +31,10 @@ for i in range(0,len(a)):     # len(a) - means length of a list
 a = [10,20,30,40,50]
 a.append(60)   # adds a value to the last spot
 a.append("Hello")
-a.insert(2,25)   # add values in the middle
-print(a) 
+print(a)     # [10,20,30,40,50,60,'Hello']
 
 # insert method -: list,insert(value) - add values in the middle.
 
 a = [10,20,40,50]
 a.insert(2,30) 
-print(a) 
+print(a)     #  [10,20,30,40,50]
