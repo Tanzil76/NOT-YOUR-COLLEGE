ecc3e9402804b75968f616d786434da47b3d6031
Tanzil <tanzilibrahim5@gmail.com>
2026-09-02 19:07:22 +0530

    new

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,4 @@
-# Dictionary -: keys and values
+# Dictionary -: keys and values -> print(dic[keys])
 
-d = {10:100,20:200,30:300}
+d = {10:100,20:200,30:300}       # 10,20,30 -> they are keys | 100,200,300 -> they are values of keys.
 print(d[10])
