9bd5b29359a0f010f84431c121070281627d5651
Tanzil <tanzilibrahim5@gmail.com>
2026-09-02 19:13:35 +0530

    new

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,12 @@
-# Dictionary -: keys and values -> print(dic[keys])
+# Dictionary -: keys and values -> print(dic[keys]) -> {key:value}
+   # Vanila python -:
 
-d = {10:100,20:200,30:300}       # 10,20,30 -> they are keys | 100,200,300 -> they are values of keys.
-print(d[10])
+d = {10:100,20:200,30:300,40:400,50:500}       # 10,20,30,40 -> they are keys | 100,200,300,400 -> they are values of keys.
+print(d[10])                     # 100
+
+# creating a new key value pair -:
+
+d[50] = 500
+print(d)                         # {10:100,20:200,30:300,40:400,50:500}
+
+# 
