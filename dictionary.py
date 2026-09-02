9a168ca8e599718621044392403d476973f0ff1d
Tanzil <tanzilibrahim5@gmail.com>
2026-09-02 19:19:46 +0530

    new

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,22 @@
-# Dictionary -: keys and values -> print(dic[keys]) -> {key:value}
-   # Vanila python -:
+# Dictionary -: keys and values -> {key:value}
 
-d = {10:100,20:200,30:300,40:400,50:500}       # 10,20,30,40 -> they are keys | 100,200,300,400 -> they are values of keys.
+# Vanilla python -:
+
+# 1. reading a value -:
+
+d = {10:100,20:200,30:300,40:400}       # 10,20,30,40 -> they are keys | 100,200,300,400 -> they are values of keys.
 print(d[10])                     # 100
 
-# creating a new key value pair -:
+# 2. creating a new key value pair -:
 
 d[50] = 500
 print(d)                         # {10:100,20:200,30:300,40:400,50:500}
 
-# 
+# 3. updating a key value that already exist -:
+
+d = {10:11,20:200,30:300,40:400} 
+d[10] = 100
+print(d)
+
+# Methods Approach -:
+
