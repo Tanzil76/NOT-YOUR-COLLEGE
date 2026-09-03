299e4b99355658c4766f3ce45adbd15a4b0d6af5
Tanzil <tanzilibrahim5@gmail.com>
2026-09-03 20:29:38 +0530

    new

--- filehandlin.py
+++ filehandlin.py
@@ -1,4 +1,21 @@
 # File Handling -: 
+# File modes -:
+
+  # 'r' -: read only
+
+ # they create files -:
+  # 'w' -: write(overwrite!) | 'a' -: append to end | 'x' -: create(fails if exists)
 
 # This will create a file.
-#open("Hello.txt","x") 
+#open("Hello.txt","x")
+
+# This helps to write data in file -:
+
+# file = open("Python.txt","w")
+
+# data = input("What you want to write in your file -: ")
+
+# file.write(data)
+
+file = open("Python.txt","r")
+print(file.read())
