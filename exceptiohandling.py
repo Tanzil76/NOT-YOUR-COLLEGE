bab6111ff99f4a7dd5e14e60661dc3c4efd9adf7
Tanzil <tanzilibrahim5@gmail.com>
2026-09-03 19:55:03 +0530

    new

--- exceptiohandling.py
+++ exceptiohandling.py
@@ -3,6 +3,10 @@
 a = int(input("Please give 1st number -: "))
 b = int(input("Please give 2nd number -: "))
 
+# in python code runs line by line means - agr koi line mai error hai toh aage ka line execute nhi hoga isliye.
+# isme hum log "try and except" use krte hai ki aage ka line execute ho jaaye agr koi error hai toh.
+# aap yeh wale code mai dekh skte hai isme aisa hi hua hai. 
+
 try:
     print(a/b)
 except Exception as err:
