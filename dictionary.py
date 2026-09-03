7b5adad1dea85afc4d278d9c23f10a29c937fd4a
Tanzil <tanzilibrahim5@gmail.com>
2026-09-03 14:10:31 +0530

    new

--- dictionary.py
+++ dictionary.py
@@ -84,7 +84,7 @@ print(d1)
 # sbse pehle d2 mai i chalega d pr 
 # pr fir uske baad d1[i] -> d1[d] - yeh new key ban gya d1 mai
 #  fir d2[i] -> d2[d] = 40 ho jayega iska mtlb jo naya key bana hai usme d1[d] usme 40 assign ho jayega 
-# fir krkte krte sb ho jayeange vice-versa. 
+# fir ek ek krke sb ho jayega vice-versa.
 # print hoga {'a':10,'b':20,'c':30,'d':40,'e':50,'f':60} 
 
 # 2. Sum all values in a dictionary.
