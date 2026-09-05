0e2581ac2092eddbdcfbecbcd555698c523cdeb1
Tanzil <tanzilibrahim5@gmail.com>
2026-09-05 21:08:57 +0530

    new

--- oops.py
+++ oops.py
@@ -63,3 +63,24 @@ obj2 = Bags("Polyster" , 3 , 4)
 # Accessing the attribute.
 print(obj1.material)
 print(obj2.material)
+
+# 4. Attributes and Methods -:
+
+class Animal:
+    a = 12    # Class attribute
+
+    def __init__(self,name):
+        self.name = name      # Object/Instance attribute
+
+    def hello(self):          # Object/Instance method - Captures the location of object.
+        print(f"How are you my name is {self.name}")
+
+    @classmethod     # Decorators
+    # jaise hi mai decorators lgata hu upar mai tbhi neeche wala jo method hai jo object ban skta tha lekin ab woh ek class ban gya hai.
+    def details(cls):         # Class method - Captures the location of class.
+        print(f"How are you my name is {cls.a}")
+
+obj = Animal("Lion")
+
+obj.hello()
+obj.details()
