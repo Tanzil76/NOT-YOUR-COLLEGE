322e87557585328abf217a63bb9e12af4d952077
Tanzil <tanzilibrahim5@gmail.com>
2026-09-05 19:53:45 +0530

    new

--- oops.py
+++ oops.py
@@ -9,3 +9,18 @@ print(a + b)
 def addition(a,b):
     print(a + b)
 addition(12,13)
+
+# OOPS -:
+
+# 1. Class -: it stores attributes and methods.
+
+class Car:
+    a = 12                             # attributes - variables that define in class is know as attributes.
+
+    def hello():                       # Methods - functions that define in class is known as methods.
+        print("Hello! How are you")
+
+# You can access attributes 
+# and methods after accessing  the class
+print(Car.a)   # accessing attributes
+Car.hello()    # accessing methods
