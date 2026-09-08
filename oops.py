42d063dad72ad4ca547e3ffe71a2f1c0b305d38e
Tanzil <tanzilibrahim5@gmail.com>
2026-09-08 20:45:37 +0530

    new

--- oops.py
+++ oops.py
@@ -283,7 +283,8 @@ obj3 = truck()
 class Animal:
     def __init__(self,name):
         self.name = name
-
+    
+    # This is dunder method -:
     def __str__(self):
         return f"Hello my name is {self.name}"
     
@@ -291,3 +292,21 @@ obj = Animal("Lion")
 obj2 = Animal("Giraffe")
 print(obj)
 print(obj2)
+
+# Solve -:   
+
+class numbers:
+    def __init__(self,num):
+        self.num = num
+
+    def __add__(self,other):
+        return self.num + other.num
+    
+    def __eq__(self,value):
+        return self.num == value.num
+    
+num1 = numbers(30)
+num2 = numbers(30)
+
+print(num1 + num2)
+print(num1 == num2)
