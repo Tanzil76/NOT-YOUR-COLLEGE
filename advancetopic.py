9dec5f070f9c1d590cc73b3ada220f9cf43bac95
Tanzil <tanzilibrahim5@gmail.com>
2026-09-10 22:09:00 +0530

    new

--- advancetopic.py
+++ advancetopic.py
@@ -58,7 +58,7 @@ print("even number") if a % 2 == 0 else print("odd number")
 
 # Comprehension -:
 
-# List Comprehension -: 
+# i. List Comprehension -: 
 
 a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
