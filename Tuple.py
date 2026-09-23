f955debc57f9060b0b6b81532a5fbbc154caccb7
Tanzil <tanzilibrahim5@gmail.com>
2026-09-23 18:53:36 +0530

    new

--- Tuple.py
+++ Tuple.py
@@ -1,5 +1,5 @@
 # Tuple -: Exactly like a list , except you cannot change it once created. It is immutable nature. Use on constant data - days of the 
-# week, coordiantes or config. values.
+# week, coordinates or config. values.
    #   0     ,     1    ,  2 ,  3 ,  4 ,  5 ,  6
 a = ["Monday", "Tuesday", 123, 567, 123, 123, 123]
 
